90e35fdaec3030011559342b01ed063f03d8c28c
1djmao <1djmao@gmail.com>
2017-07-06 10:09:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/idjmao/viewstyle/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.idjmao.viewstyle;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabLayout= (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab3"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab4"));
+    }
+}
